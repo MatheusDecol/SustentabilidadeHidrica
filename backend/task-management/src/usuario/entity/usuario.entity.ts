2898e84239a728c95c20738a9b3f092d61b97e52
Lucas Laudenir <lucaslaudenirfilipim@gmail.com>
2025-05-19 17:49:09 -0300

    Remoção da parte de equipe

--- backend/task-management/src/usuario/entity/usuario.entity.ts
+++ backend/task-management/src/usuario/entity/usuario.entity.ts
@@ -1,36 +1,26 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { UsuarioEquipeEntity } from "src/usuario_equipe/entity/usu.eq.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('usuario')
-export class UsuarioEntity{
-    
-    @PrimaryGeneratedColumn()
-    idU: number;
+export class UsuarioEntity {
+  @PrimaryGeneratedColumn()
+  idU: number;
 
-    @Column({type: 'varchar', length: 50})
-    nome: string;
+  @Column({ type: 'varchar', length: 50 })
+  nome: string;
 
-    @Column({type: 'varchar', length: 50})
-    email: string;
+  @Column({ type: 'varchar', length: 50 })
+  email: string;
 
-    @Column({type: 'varchar', length: 255})
-    senha: string;
+  @Column({ type: 'varchar', length: 255 })
+  senha: string;
 
-    @Column({type: 'boolean', default: false})
-    ativo: boolean;
+  @Column({ type: 'boolean', default: false })
+  ativo: boolean;
 
-    @OneToMany(() => UsuarioEquipeEntity, usuarioEquipe => usuarioEquipe.usuario)
-    usuarioEquipes: UsuarioEquipeEntity[];
-
-    constructor(
-        nome: string,
-        email: string,
-        senha: string,
-        ativo?: boolean
-    ) {
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.ativo = ativo;
-    }
+  constructor(nome: string, email: string, senha: string, ativo?: boolean) {
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.ativo = ativo;
+  }
 }
