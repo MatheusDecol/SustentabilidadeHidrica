f6394fede887a27668feb9f40de31504db8ea29d
Leticia Takahashi <leticiasatikotakahashi@gmail.com>
2025-06-01 23:17:35 -0300

    feat adição tela cadastro

--- frontend/scripts/cadastro.js
+++ frontend/scripts/cadastro.js
@@ -15,27 +15,25 @@ formCadastro.addEventListener("submit", async (e) => {
     
     try{
         const response = await axios.post("http://localhost:3001/usuario", body)
-        
         console.log(response);
         window.location.href = "/frontend/views/login.html";
     }catch(error){
         console.log(error)
         alert(`Erro ao cadastrar usuário: ${error.response.data.message}`)
     }
-})
+});
 
-
+const senha = document.getElementById("senha");
 const btn = document.getElementById("verSenha");
 
 btn.addEventListener('click', () => {
     if (senha.type === 'password') {
         senha.type = 'text';
-        btn.classList.add('fa-eye');
         btn.classList.remove('fa-eye-slash');
+        btn.classList.add('fa-eye');
     } else {
         senha.type = 'password';
-        btn.classList.add('fa-eye-slash');
         btn.classList.remove('fa-eye');
+        btn.classList.add('fa-eye-slash');
     }
 });
-
